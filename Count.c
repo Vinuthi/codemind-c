6794c6e16ed73ed575a1704269804982e8824dfb
Vinuthi <98702539+Vinuthi@users.noreply.github.com>
2022-04-21 20:34:19 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,o=0,e=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2)         {             o++;         }         else         {             e++;         }     }     printf("%d %d",e,o); }

---
+++ Count.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,o=0,e=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2)
+        {
+            o++;
+        }
+        else
+        {
+            e++;
+        }
+    }
+    printf("%d %d",e,o);
+}
