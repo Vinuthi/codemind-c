36d9a1a92db66118b593565724daaf70a13a3a13
Vinuthi <98702539+Vinuthi@users.noreply.github.com>
2022-07-02 13:44:54 +0530

    #include<stdio.h> int main() {     int a[100][100],n,m,sum=0;     scanf("%d%d",&n,&m);     for(int i=0;i<n;i++)     {         for(int j=0;j<m;j++)         {             scanf("%d",&a[i][j]);             sum=sum+a[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100],n,m,sum=0;
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+            sum=sum+a[i][j];
+        }
+    }
+    printf("%d",sum);
+}
